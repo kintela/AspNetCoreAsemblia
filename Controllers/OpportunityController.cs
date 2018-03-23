2f5f8cbb548c4bdb921d49851e52a4334805206f
FULCRUM\RQUINTELA <rquintela@fyseg.com>
2018-03-23 11:26:23 +0100

    refactorizar

--- Controllers/OpportunityController.cs
+++ Controllers/OpportunityController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using web.Models;
 using web.DataBase;
 using web.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace web.Controllers
 {
@@ -53,13 +54,13 @@ namespace web.Controllers
             return Ok(opportunity);
         }
 
-        [HttpPut]
-        [Route("{id}")]
+        [HttpPost]
+        [Route("{id}/accept")]
         public IActionResult Accept(int id)
         {
             var context = new FysegContext();
 
-            var opportunity = context.Opportunities.SingleOrDefault(o => o.Id == id);
+            var opportunity = context.Opportunities.Include(o=>o.LeadEmployee).SingleOrDefault(o => o.Id == id);
 
             if (opportunity == null)
             {
@@ -68,20 +69,44 @@ namespace web.Controllers
 
             opportunity.State = "Accepted";
 
+            //realmente se actualizan todas las propiedades no solo State
+            context.Update(opportunity);
             context.SaveChanges();
 
             var mailServive = new MailService();
 
-            var leadMail = context.Employees.SingleOrDefault(e => e.Id == opportunity.LeadEmployeeId).Email;
-
-            mailServive.SendMail(leadMail);
+            mailServive.SendMail(opportunity.LeadEmployee.Email);
 
             var tender = new Tender()
             {
                 Title=opportunity.Title                
             };
 
-            RedirectToActionResult redirectResult = new RedirectToActionResult("Add", "TenderController", tender);
+            context.Tenders.Add(tender);
+
+            //provoca error de referencia circular al devover opportunity que a su vez busca empleados y que a su vez busca oportunidades, etc...
+            //nunca devlver en una API el modelo de la bbdd o de dominio sino un view model
+            return Ok(opportunity);
+        }
+
+
+        [HttpPost]
+        [Route("{id}/accept2")]
+        public IActionResult Accept2(int id)
+        {
+            var context = new FysegContext();
+
+            var opportunity = new Opportunity()
+            {
+                Id=id,
+                State="accepted"
+            };
+
+            var entry = context.Attach(opportunity);
+
+            entry.Property(o => o.State).IsModified=true;
+
+            context.SaveChanges();
 
             return Ok(opportunity);
         }
--- DataBase/FysegContext.cs
+++ DataBase/FysegContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,10 @@ namespace web.DataBase
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
-                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FysegDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FysegDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .UseLoggerFactory(new LoggerFactory(new List<ILoggerProvider>() { new ConsoleLoggerProvider((s,l)=>true,true)}));
+
             }
     }
 }
